dca7b3724d41e052936d3fdc6d50b445c1d3baeb
Stewart Megaw <stew@glide.design>
2017-09-26 12:51:35 +0100

    u

--- partials/nestedCollection/list.jsx
+++ partials/nestedCollection/list.jsx
@@ -42,10 +42,30 @@ import styles from '../../style/list.css';
 
 const List = React.createClass({
 	getInitialState:function(){
+		var collapsed = [];
+		var _this = this;
+
+		function getDefaultCollapsed(listArray, root) {
+			for(var j = 0; j < listArray.length; j++)
+			{
+				if(listArray[j].constructor !== Array)
+				{
+					var hasChildren = listArray.length > j + 1 && listArray[j+1].constructor === Array;
+					if(_this.props.showToggles && hasChildren && !root)
+						collapsed.push(listArray[j].id);
+					if(hasChildren)
+						getDefaultCollapsed(listArray[j+1]);
+				}
+			}
+
+		}
+		getDefaultCollapsed(this.props.listArray, true);
+
 		return {
-			collapsed:[],
+			collapsed:collapsed,
 			checked:this.props.checked || [],
 			disabled:[],
+			hidden:{} // used in conjunction with p.hideMoreThan
 		}
 	},
 	componentDidMount(){
@@ -153,84 +173,109 @@ const List = React.createClass({
 
 		for(var j = 0; j < listArray.length; j++)
 		{
-			if(listArray[j].constructor !== Array)
-			{
-				(function(listArray, j){
-					var newRouteLabels = routeLabels.slice(0);					
-					newRouteLabels.push(listArray[j].routeLabel);
 
-					var hasChildren = listArray.length > j + 1 && listArray[j+1].constructor === Array;
+				if(listArray[j].constructor !== Array)
+				{
+					(function(listArray, j){
+						var newRouteLabels = routeLabels.slice(0);					
+						newRouteLabels.push(listArray[j].routeLabel);
 
-					items.push(
-						<li
-							data-position={p.positionField ? listArray[j][p.positionField] : null}
-							key={listArray[j].id}
-							style={p.itemStyle}
-							className={p.listItemClass}
-						>
-							{p.showToggles && hasChildren ?
-								<span
-	                                className={"toggle "+ (s.collapsed.indexOf(listArray[j].id) != -1 ? "up" : "")}
-	                                onClick={(e)=>{
-	                                    _this.toggleItem(listArray[j].id);
-	                                }}
-	                            />
-                            :null}
+						var hasChildren = listArray.length > j + 1 && listArray[j+1].constructor === Array;
 
-							{p.showCheckboxes && !(p.parentStatic && depth==0 && j==0) ?
-								<input
-									ref={"checkbox"+listArray[j].id}
-									type="checkbox"
-									name={p.checkboxName ? p.checkboxName + "[]" : "checkedboxes[]"}
-									onChange={(e)=>{
-										if(p.onCheckboxChange)
-										{
-											if(p.onCheckboxChange(e) !== false)
-												_this.onCheckboxChange(listArray[j].id);
+						var childItems = hasChildren && s.collapsed.indexOf(listArray[j].id) == -1 ? _this.get_list_item(listArray[j+1], newRouteLabels, depth+1) : [];
+
+						items.push(
+							<li
+								data-position={p.positionField ? listArray[j][p.positionField] : null}
+								key={listArray[j].id}
+								style={p.itemStyle}
+								className={p.listItemClass}
+							>
+								{p.showToggles && hasChildren ?
+									<span
+		                                className={"toggle "+ (s.collapsed.indexOf(listArray[j].id) != -1 ? "up" : "")}
+		                                onClick={(e)=>{
+		                                    _this.toggleItem(listArray[j].id);
+		                                }}
+		                            />
+	                            :null}
+
+	                            <div className={p.listItemContainerClass}>
+									{p.showCheckboxes && !(p.parentStatic && depth==0 && j==0) ?
+										<input
+											ref={"checkbox"+listArray[j].id}
+											type="checkbox"
+											name={p.checkboxName ? p.checkboxName + "[]" : "checkedboxes[]"}
+											onChange={(e)=>{
+												if(p.onCheckboxChange)
+												{
+													if(p.onCheckboxChange(e) !== false)
+														_this.onCheckboxChange(listArray[j].id);
+												}
+												else
+													_this.onCheckboxChange(listArray[j].id);
+											}}
+											value={listArray[j].id}
+											checked={s.checked.indexOf(listArray[j].id.toString())==-1 ? false : true}
+											disabled={s.disabled.indexOf(listArray[j].id.toString())==-1 ? false : 'disabled'}
+										/>
+									: null}
+
+									<span onClick={p.parentStatic && depth==0 && j==0 ? null : _this.contentClicked.bind(null, listArray[j].id)}>
+										{p.liContent ? p.liContent(
+												listArray[j],
+												s.checked.indexOf(listArray[j].id.toString())!=-1,
+												s.disabled.indexOf(listArray[j].id.toString())!=-1
+											)
+										:
+											<Link
+												to={'/help/'+newRouteLabels.join('/')}
+												activeClassName={styles.activeLink}
+												className={_this.props.linkClass}
+												onClick={(e)=>{
+													window.location = '/help/'+newRouteLabels.join('/');
+													e.stopPropagation();
+												}}
+											>
+												{listArray[j].title}
+											</Link>
 										}
-										else
-											_this.onCheckboxChange(listArray[j].id);
-									}}
-									value={listArray[j].id}
-									checked={s.checked.indexOf(listArray[j].id.toString())==-1 ? false : true}
-									disabled={s.disabled.indexOf(listArray[j].id.toString())==-1 ? false : 'disabled'}
-								/>
-							: null}
+									</span>
+								</div>
 
-							<span onClick={p.parentStatic && depth==0 && j==0 ? null : _this.contentClicked.bind(null, listArray[j].id)}>
-								{p.liContent ? p.liContent(
-										listArray[j],
-										s.checked.indexOf(listArray[j].id.toString())!=-1,
-										s.disabled.indexOf(listArray[j].id.toString())!=-1
-									)
-								:
-									<Link
-										to={'/help/'+newRouteLabels.join('/')}
-										activeClassName={styles.activeLink}
-										className={_this.props.linkClass}
-										onClick={(e)=>{
-											window.location = '/help/'+newRouteLabels.join('/');
-											e.stopPropagation();
-										}}
-									>
-										{listArray[j].title}
-									</Link>
-								}
-							</span>
+								{hasChildren && s.collapsed.indexOf(listArray[j].id) == -1 ?
+									<span>
+										<ul
+											style={_this.props.listStyle}
+											className={_this.props.listClass}
+										>
+											{p.hideMoreThan && childItems.length > p.hideMoreThan && !s.hidden['_'+listArray[j].id] ? 
+												childItems.slice(0, p.hideMoreThan)
+											:
+												childItems
+											}
+										</ul>
+										{p.hideMoreThan && childItems.length > p.hideMoreThan && !s.hidden['_'+listArray[j].id] ?
+											<a
+												href="#"
+												className={styles.more}
+												onClick={(e)=>{
+													var hidden = Object.assign(s.hidden);
+													hidden['_'+listArray[j].id] = true;
+													_this.setState({hidden:hidden});
+													e.preventDefault();
+												}}
+											>
+												More
+											</a>
+										:null}
+									</span>
+								:null}
+							</li>
+						);
 
-							{hasChildren && s.collapsed.indexOf(listArray[j].id) == -1 ?
-								<ul
-									style={_this.props.listStyle}
-									className={_this.props.listClass}
-								>
-									{_this.get_list_item(listArray[j+1], newRouteLabels, depth+1)}
-								</ul>
-							:null}
-						</li>
-					);
-
-				})(listArray, j);
-			}
+					})(listArray, j);
+				}
 		}
 
 		if(p.positionField)
@@ -239,14 +284,20 @@ const List = React.createClass({
 		return items;
 	},
     render() {
+    	var p = this.props;
+    	var childItems = this.get_list_item(p.listArray, null, 0);
 
 		return (
-			<ul
-				style={Object.assign({}, this.props.listStyle || {}, this.props.outerListStyle || {})}
-				className={this.props.outerListClass}
-			>
-				{this.get_list_item(this.props.listArray, null, 0)}
-			</ul>
+			<span>
+				<ul
+					style={Object.assign({}, p.listStyle || {}, p.outerListStyle || {})}
+					className={p.outerListClass}
+				>
+					{childItems}
+				</ul>
+				{/* 'Show more' not needed in the tree root yet 
+				p.hideMoreThan && childItems.length > p.hideMoreThan ? <a href="#" className={styles.more}>More</a>:null*/}
+			</span>
 		);
 	}
 });
